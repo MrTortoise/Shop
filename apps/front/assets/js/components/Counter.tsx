061beed4a2de5e50664010d224b11f52f44591cf
John Nicholas <themrtortoise@gmail.com>
2018-08-05 13:04:04 +0100

    counter working

---
+++ apps/front/assets/js/components/Counter.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import {Link} from 'react-router-dom'
+import {Button} from 'reactstrap'
+
+interface CounterState {
+    currentCount: number
+}
+
+const initialState = {currentCount: 0}
+
+export default class Counter extends React.Component<{}, CounterState> {
+    constructor(){
+        super()
+
+        this.state = initialState
+    }
+
+    public render(): JSX.Element{
+        return (
+            <div>
+                <h1>Counter</h1>
+                <p>Dead simple react component</p>
+                <p>Current Count: <strong>{this.state.currentCount}</strong></p>
+
+                        <Button color="primary" onClick={() => { this.incrementCounter() }}>Increment counter</Button>{' '}
+                <Button color="secondary" onClick={() => { this.decrementCounter() }}>Decrement counter</Button>{' '}
+                <Button color="secondary" onClick={() => { this.resetCounter() }}>Reset counter</Button>
+                <br /><br />
+                <p><Link to="/">Back to home</Link></p>                
+            </div>
+        )
+    }
+
+    private incrementCounter(){
+        this.setState({
+            currentCount: this.state.currentCount + 1
+        })
+    }
+
+    private decrementCounter(){
+        this.setState({
+            currentCount: this.state.currentCount-1
+        })
+    }
+
+    private resetCounter(){
+        this.setState({
+            currentCount: 0
+        })
+    }
+}
